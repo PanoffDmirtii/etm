d075808ae19d514ec6580ada935a923251bae9b4
xs <panov.alchemic@gmail.com>
2020-07-10 15:55:28 +0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { Navbar } from 'react-bootstrap'
 export const Header = (props) => {
   return (
     <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="/">Главная</Navbar.Brand>
-      <Navbar.Brand href="/catalog">Каталог</Navbar.Brand>
+      <Navbar.Brand href="/etm">Главная</Navbar.Brand>
+      <Navbar.Brand href="/etm/catalog">Каталог</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
