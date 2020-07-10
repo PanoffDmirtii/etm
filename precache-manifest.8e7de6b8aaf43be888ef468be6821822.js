bd3f436476371e0a1e18bfe9eaa8fac26ff37003
xs <panov.alchemic@gmail.com>
2020-07-10 15:44:10 +0300

    Updates

---
+++ precache-manifest.8e7de6b8aaf43be888ef468be6821822.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678979abc91a250cda11cad96423a377",
+    "url": "/etm/index.html"
+  },
+  {
+    "revision": "deca8ff984207274ccc3",
+    "url": "/etm/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2c911b6c82c8a52d06f1",
+    "url": "/etm/static/css/main.289dda88.chunk.css"
+  },
+  {
+    "revision": "deca8ff984207274ccc3",
+    "url": "/etm/static/js/2.7cd13039.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/etm/static/js/2.7cd13039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c911b6c82c8a52d06f1",
+    "url": "/etm/static/js/main.927f4e15.chunk.js"
+  },
+  {
+    "revision": "b3dbcdf3a4673b7d29b3",
+    "url": "/etm/static/js/runtime-main.8f53d604.js"
+  }
+]);
